a918d625cd1ffe1c5fc91df275bc16b6d8a5570f
IMANMONIRI <168808287+IMANMONIRI@users.noreply.github.com>
2024-05-18 06:50:04 +0330

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,26 @@
 import { login, signup, google } from "./actions";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <form className="flex flex-col justify-center items-center gap-y-4">
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-      <button formAction={google}>Google</button>
-    </form>
+    <div className="absolute container top-[10vh] left-0 w-full h-[90vh] flex justify-center items-center">
+        <form className="bg-cyan-950/50 w-full sm:w-2/3 md:w-1/2 lg:w-1/3 border-2 border-gray-800 p-8 rounded-2xl flex flex-col justify-center items-start gap-y-4">
+          <div className="w-full">
+            <Label htmlFor="email">Email:</Label>
+            <Input className="w-full" id="email" name="email" type="email" placeholder="Email..." />
+          </div>
+          <div className="w-full">
+            <Label htmlFor="password">Password:</Label>
+            <Input className="w-full" id="password" name="password" type="password" placeholder="Password..." />
+          </div>
+          <div className="w-full flex justify-center items-center gap-x-4">
+            <Button className="w-full hover:bg-cyan-600" variant="outline" formAction={login}>Log in</Button>
+            <Button className="w-full hover:bg-cyan-600" variant="outline" formAction={signup}>Sign up</Button>
+          </div>
+          <Button className="w-full hover:bg-cyan-600" variant="outline" formAction={google}>Google</Button>
+        </form>
+      </div>
   );
 }
